fa440cd9805ab9ab915406e573633403d6edde0f
theshittywizard <theharrywizard@gmail.com>
2016-11-04 12:58:50 +0100

    Added Ability to read from saved pictures.

--- app/src/main/java/com/domain/thomas/readit_tesseract/MainActivity.java
+++ app/src/main/java/com/domain/thomas/readit_tesseract/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.io.IOException;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
@@ -23,11 +25,14 @@ public class MainActivity extends Activity {
     private static final String DESCRIPTION = "Ein mit der App tesseracttest aufgenommenes Foto";
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int IMAGE_CAPTURE = 1;
+    private static final int IMG_RESULT = 2;
 
     private ImageView imageView;
     private TextView edittextView;
+    private Button LoadImage;
+    private Button GetImage;
 
-    private Uri imageUri;
+    private Uri imageURI;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,12 +42,22 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         imageView = (ImageView) findViewById(R.id.view);
+        LoadImage = (Button) findViewById(R.id.buttonGetPicture);
+        GetImage = (Button) findViewById(R.id.buttonTakePicture);
         edittextView = (TextView) findViewById(R.id.editText);
 
-        Button button = (Button) findViewById(R.id.shoot);
+        LoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+                startActivityForResult(intent, IMG_RESULT);
+            }
+        });
+        GetImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 startCamera();
             }
         });
@@ -52,53 +67,88 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == IMAGE_CAPTURE) {
-            if (resultCode == RESULT_OK) {
-                try {
+        if (requestCode == IMG_RESULT && resultCode == RESULT_OK
+                && null != data) {
 
-                    final Bitmap b1 = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+            imageURI = data.getData();
 
-                    float w1 = b1.getWidth();
-                    float h1 = b1.getHeight();
+            Bitmap b1 = null;
+            try {
+                b1 = MediaStore.Images.Media.getBitmap(getContentResolver(), imageURI);
+            } catch (IOException e) {
+                Toast.makeText(this, "Something bad happened while getting a bitmap. (1)", Toast.LENGTH_LONG)
+                        .show();
+            }
 
-                    int h2 = 300;
-                    int w2 = (int) (w1 / h1 * (float) h2);
+            final Bitmap bt = b1;
 
-                    Bitmap b2 = Bitmap.createScaledBitmap(b1, w2, h2, false);
-                    imageView.setImageBitmap(b2);
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    final TessBaseAPI bildOcr = new TessBaseAPI();
+                    bildOcr.init("storage/emulated/0/","deu");
+                    bildOcr.setImage(bt);
+                    Log.d(TAG, "Bild gesetzt");
 
-                    Runnable r = new Runnable() {
+                    final String readText = bildOcr.getUTF8Text();
+
+                    runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            final TessBaseAPI bildOcr = new TessBaseAPI();
-                            bildOcr.init("storage/emulated/0/","deu");
-                            bildOcr.setImage(b1);
-                            Log.d(TAG, "Bild gesetzt");
-
-                            final String readText = bildOcr.getUTF8Text();
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    edittextView.setText(readText);
-                                }
-                            });
-
-                            Log.d(TAG, "Text gelesen");
-                            bildOcr.end();
-                            Log.d(TAG, "OCR beendet");
+                            edittextView.setText(readText);
                         }
-                    };
-                    Thread t = new Thread(r);
-                    t.start();
+                    });
 
-                } catch (IOException e) {
-                    Log.e(TAG, "setBitmap()", e);
+                    Log.d(TAG, "Text gelesen");
+                    bildOcr.end();
+                    Log.d(TAG, "OCR beendet");
                 }
-            } else {
-                int rowsDeleted = getContentResolver().delete(imageUri, null, null);
-                Log.d(TAG, rowsDeleted + " rows deleted");
+            };
+            Thread t = new Thread(r);
+            t.start();
+
+            Bitmap b2 = bmDownscale(b1, 480);
+            imageView.setImageBitmap(b2);
+
+        } else if (requestCode == IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bitmap b1 = null;
+            try {
+                b1 = MediaStore.Images.Media.getBitmap(getContentResolver(), imageURI);
+            } catch (IOException e) {
+                Toast.makeText(this, "Something bad happened while getting a bitmap. (2)", Toast.LENGTH_LONG)
+                        .show();
             }
+
+            final Bitmap bt = b1;
+
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    final TessBaseAPI bildOcr = new TessBaseAPI();
+                    bildOcr.init("storage/emulated/0/","deu");
+                    bildOcr.setImage(bt);
+                    Log.d(TAG, "Bild gesetzt");
+
+                    final String readText = bildOcr.getUTF8Text();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            edittextView.setText(readText);
+                        }
+                    });
+
+                    Log.d(TAG, "Text gelesen");
+                    bildOcr.end();
+                    Log.d(TAG, "OCR beendet");
+                }
+            };
+            Thread t = new Thread(r);
+            t.start();
+
+            Bitmap b2 = bmDownscale(b1, 480);
+            imageView.setImageBitmap(b2);
         }
     }
 
@@ -108,9 +158,18 @@ public class MainActivity extends Activity {
         values.put(MediaStore.Images.Media.TITLE, TITLE);
         values.put(MediaStore.Images.Media.DESCRIPTION, DESCRIPTION);
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        imageURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageURI);
         startActivityForResult(intent, IMAGE_CAPTURE);
+    }
+
+    public Bitmap bmDownscale(Bitmap bm, int hResTarget) {
+        float w1 = bm.getWidth();
+        float h1 = bm.getHeight();
+
+        int w2 = (int) (w1 / h1 * (float) hResTarget);
+
+        return Bitmap.createScaledBitmap(bm, w2, hResTarget, false);
     }
 }
