9e036cfa16e01c5c0d38c867058a578409b37040
quantumpp <jonasrohrauer@gmail.com>
2017-02-24 15:26:30 +0100

    loading the spinner

--- app/src/main/java/com/domain/thomas/readit_tesseract/EndActivity.java
+++ app/src/main/java/com/domain/thomas/readit_tesseract/EndActivity.java
@@ -6,7 +6,11 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -14,10 +18,12 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.domain.thomas.readit_tesseract.EditImageActivity.rawText;
 
-public class EndActivity extends AppCompatActivity {
+public class EndActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String dirName = "ReadIt";
     String parentDirName = Environment.DIRECTORY_DOCUMENTS;
@@ -29,6 +35,7 @@ public class EndActivity extends AppCompatActivity {
     private Button saveFile;
     private TextView filename;
     private Button saveButton;
+    private Spinner mimeSelection;
 
 
     @Override
@@ -44,11 +51,21 @@ public class EndActivity extends AppCompatActivity {
         saveFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 final Dialog dialog = new Dialog(context);
                 dialog.setContentView(R.layout.dialog_save);
 
                 saveButton = (Button) dialog.findViewById(R.id.buttonSaveConfirm);
                 filename = (TextView) dialog.findViewById(R.id.filename);
+                mimeSelection = (Spinner) dialog.findViewById(R.id.spinnerDataType);
+
+                List<String> mimeTypes = new ArrayList();
+
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,
+                        R.array.mime_type_array, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                mimeSelection.setAdapter(adapter);
 
                 saveButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -68,9 +85,19 @@ public class EndActivity extends AppCompatActivity {
         editText.setText(rawText);
     }
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 
     private void saveFile() {
-
         if (isExternalStorageWritable()) {
             //File file = getAlbumStorageDir("ReadIt");
             String text = editText.getText().toString();
@@ -79,18 +106,16 @@ public class EndActivity extends AppCompatActivity {
 
             Toast.makeText(this, "file saved as " + fileName, Toast.LENGTH_LONG).show();
         }
-
     }
 
     public void generateNoteOnSD(String parentDir, String dirName, String fileName, String text) {
         try {
-            File file = new File(Environment.getExternalStoragePublicDirectory(parentDir), dirName);
-
-            if (!file.exists()) {
-                file.mkdirs();
+            File dirFile = new File(Environment.getExternalStoragePublicDirectory(parentDir), dirName);
+            if (!dirFile.exists()) {
+                dirFile.mkdirs();
             }
-            File gpxfile = new File(file, fileName);
-            FileWriter writer = new FileWriter(gpxfile);
+            File file = new File(dirFile, fileName);
+            FileWriter writer = new FileWriter(file);
             writer.append(text);
             writer.flush();
             writer.close();
